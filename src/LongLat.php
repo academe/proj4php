a7b7b74b7bca40a1c2d51f2d2a9591e35468fd04
Jason Judge <jason.judge@consil.co.uk>
2014-06-23 12:27:06 +0100

    first commit

---
+++ src/LongLat.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Academe\Proj4Php;
+
+/**
+ * Author : Julien Moquet, Jason Judge
+ * 
+ * Inspired by Proj4js from Mike Adair madairATdmsolutions.ca
+ *                      and Richard Greenwood rich@greenwoodmap.com 
+ * License: LGPL as per: http://www.gnu.org/copyleft/lesser.html 
+ */
+class LongLat {
+
+    public function init()
+    {
+    }
+
+    public function forward($pt)
+    {
+        return $pt;
+    }
+
+    public function inverse($pt)
+    {
+        return $pt;
+    }
+}
